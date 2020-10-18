9a8ae2065160d31d9cf7cf081412587051fc5262
Yutong Zhang <zhangyt2000@gmail.com>
2020-10-17 21:33:43 -0700

    Complete centering and scaling

--- PointCloud.cpp
+++ PointCloud.cpp
@@ -1,6 +1,5 @@
 #include "PointCloud.h"
 
-
 PointCloud::PointCloud(std::string objFilename, GLfloat pointSize) 
 	: pointSize(pointSize)
 {
@@ -9,7 +8,6 @@ PointCloud::PointCloud(std::string objFilename, GLfloat pointSize)
 	 * Modify this to read points from an obj file.
 	 */
 	std::ifstream objFile(objFilename);
-	std::cerr << "Reading " << objFilename << std::endl;
 
 	if (objFile.is_open()) {
 		std::string line;
@@ -21,9 +19,9 @@ PointCloud::PointCloud(std::string objFilename, GLfloat pointSize)
                   ss >> label;
 
                   if (label == "v") {
+				// write position to a vec3 and add to points vector
                         glm::vec3 vertex;
                         ss >> vertex.x >> vertex.y >> vertex.z;
-                        // std::cerr << "\r" << vertex.x << " "<< vertex.y << " " << vertex.z;
                         points.push_back(vertex);
                   }
 		}
@@ -31,16 +29,64 @@ PointCloud::PointCloud(std::string objFilename, GLfloat pointSize)
 	else {
 		std::cerr << "Can't open the file: " << objFilename << std::endl;
 	}
-	std::cerr << "\nFinish reading " << objFilename << std::endl;
-
 
 	/*
 	 * TODO: Section 4, you will need to normalize the object to fit in the
 	 * screen. 
 	 */
+	GLfloat xMax = points[0].x;
+	GLfloat xMin = points[0].x;
+	GLfloat yMax = points[0].y;
+	GLfloat yMin = points[0].y;
+	GLfloat zMax = points[0].z;
+	GLfloat zMin = points[0].z;
 
-	// Set the model matrix to an identity matrix. 
-	model = glm::mat4(1);
+	// iterate through all points, record max and min of 3 dimensions
+	for (const auto &vertex : points) {
+		if (vertex.x > xMax) {
+			xMax = vertex.x;
+		}
+		if (vertex.x < xMin) {
+			xMin = vertex.x;
+		}
+		if (vertex.y > yMax) {
+			yMax = vertex.y;
+		}
+		if (vertex.y < yMin) {
+			yMin = vertex.y;
+		}
+		if (vertex.z > zMax) {
+			zMax = vertex.z;
+		}
+		if (vertex.z < zMin) {
+			zMin = vertex.z;
+		}
+	}
+	
+	// calculate center coordinate
+	auto center = glm::vec3((xMin + xMax) / 2, (yMin + yMax) / 2, (zMin + zMax) / 2);
+
+	// record the maximum distance from the center
+	GLfloat distMax = 0;
+	for (const auto &vertex : points) {
+		GLfloat dist = glm::length(vertex - center);
+		if (dist > distMax) {
+			distMax = dist;
+		}
+	}
+	// calculate the scale factor, normalizing maximum distance to 9
+	GLfloat scaleFactor = 9.0 / distMax;
+
+	// create the transformation matrix
+	auto transform = glm::mat4(1);
+
+	// translate by scale factor times the negative value of the center coordinate
+	// scale by scale factor
+	transform = glm::translate(transform, - scaleFactor * center);
+	transform = glm::scale(transform, glm::vec3(scaleFactor));
+
+	// Set the model matrix to the transformation matrix
+	model = transform;
 
 	// Set the color. 
 	color = glm::vec3(1, 0, 0);
